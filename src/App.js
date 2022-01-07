c4ca884ad2c22c15dffcd2b4f015fc91bea9a28a
tplamondon <tplamondon@tutanota.de>
2022-01-06 17:57:42 -0800

    added bluetext and clicky cursor on buttons

--- src/App.js
+++ src/App.js
@@ -10,7 +10,50 @@ function App() {
 
   const appContainor = useRef(null);
   const introButton = useRef(null);
+  const workButton = useRef(null);
+  const aboutButton = useRef(null);
+  const contactButton = useRef(null);
+  const homeButton = useRef(null);
 
+  function addBlueText(e, articleButton) {
+    //at this moment, I can't figure out how to blur the background
+    /*if(appContainor.current.classList.contains("blurImage")){
+		appContainor.current.classList.remove("blurImage");
+	}
+	*/
+    if (articleButton == 0) {
+      homeButton.current.classList.add("blueText");
+    }
+    if (articleButton === 1) {
+      introButton.current.classList.add("blueText");
+    } else if (articleButton === 2) {
+      workButton.current.classList.add("blueText");
+    } else if (articleButton === 3) {
+      aboutButton.current.classList.add("blueText");
+    } else if (articleButton === 4) {
+      contactButton.current.classList.add("blueText");
+    }
+  }
+
+  function removeBlueText(e, articleButton) {
+    //at this moment, I can't figure out how to blur the background
+    /*if(appContainor.current.classList.contains("blurImage")){
+		appContainor.current.classList.remove("blurImage");
+	}
+	*/
+    if (articleButton == 0) {
+      homeButton.current.classList.remove("blueText");
+    }
+    if (articleButton === 1) {
+      introButton.current.classList.remove("blueText");
+    } else if (articleButton === 2) {
+      workButton.current.classList.remove("blueText");
+    } else if (articleButton === 3) {
+      aboutButton.current.classList.remove("blueText");
+    } else if (articleButton === 4) {
+      contactButton.current.classList.remove("blueText");
+    }
+  }
   function toggleArticle(e, articleToShow) {
     //at this moment, I can't figure out how to blur the background
     /*if(appContainor.current.classList.contains("blurImage")){
@@ -49,20 +92,30 @@ function App() {
 
         <div className="gridLinks">
           <div className="box">
-            <span onClick={(event) => toggleArticle(event, 1)}>INTRO</span>
+            <span ref={introButton} onClick={(event) => toggleArticle(event, 1)} onMouseOver={(event) => addBlueText(event, 1)} onMouseOut={(event) => removeBlueText(event, 1)}>
+              INTRO
+            </span>
           </div>
           <div className="box">
-            <span onClick={(event) => toggleArticle(event, 2)}>WORK</span>
+            <span ref={workButton} onClick={(event) => toggleArticle(event, 2)} onMouseOver={(event) => addBlueText(event, 2)} onMouseOut={(event) => removeBlueText(event, 2)}>
+              WORK
+            </span>
           </div>
           <div className="box">
-            <span onClick={(event) => toggleArticle(event, 3)}>ABOUT</span>
+            <span ref={aboutButton} onClick={(event) => toggleArticle(event, 3)} onMouseOver={(event) => addBlueText(event, 3)} onMouseOut={(event) => removeBlueText(event, 3)}>
+              ABOUT
+            </span>
           </div>
           <div className="box">
-            <span onClick={(event) => toggleArticle(event, 4)}>CONTACT</span>
+            <span ref={contactButton} onClick={(event) => toggleArticle(event, 4)} onMouseOver={(event) => addBlueText(event, 4)} onMouseOut={(event) => removeBlueText(event, 4)}>
+              CONTACT
+            </span>
           </div>
           {article != 0 && (
             <div className="box boxTake2">
-              <span onClick={(event) => toggleArticle(event, 0)}>HOME</span>
+              <span ref={homeButton} onClick={(event) => toggleArticle(event, 0)} onMouseOver={(event) => addBlueText(event, 0)} onMouseOut={(event) => removeBlueText(event, 0)}>
+                HOME
+              </span>
             </div>
           )}
         </div>
